6adef01098780d8068755eb29a7bb549589ba33c
Samuel Sfeir <samuel1808@hotmail.com>
2023-11-30 10:53:06 -0300

    adiciona requisito 03

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,48 +3,37 @@ import React, { useState } from 'react';
 function Form() {
   const [exibirFormulario, setExibirFormulario] = useState(false);
 
-  const mostrarFormulario = () => {
-    setExibirFormulario(true);
+  const toggleFormulario = () => {
+    setExibirFormulario(!exibirFormulario);
+  };
+
+  const cancelarFormulario = () => {
+    setExibirFormulario(false);
   };
 
   return (
     <div>
-      <button onClick={ mostrarFormulario }>Cadastrar nova senha</button>
+      {!exibirFormulario && (
+        <button onClick={ toggleFormulario }>Cadastrar nova senha</button>
+      )}
+
       {exibirFormulario && (
         <form className="form">
+          {/* Conteúdo do seu formulário aqui */}
           <label htmlFor="nome-do-servico">Nome do serviço</label>
-          <input
-            type="text"
-            name="nome-do-servico"
-            id="nome-do-servico"
-          />
+          <input type="text" name="nome-do-servico" id="nome-do-servico" />
 
           <label htmlFor="login">Login</label>
-          <input
-            type="text"
-            name="login"
-            id="login"
-          />
+          <input type="text" name="login" id="login" />
 
           <label htmlFor="senha">Senha</label>
-          <input
-            type="password"
-            name="senha"
-            id="senha"
-          />
+          <input type="password" name="senha" id="senha" />
 
           <label htmlFor="URL">URL</label>
-          <input
-            type="text"
-            name="URL"
-            id="URL"
-          />
+          <input type="text" name="URL" id="URL" />
 
-          <button name="Cadastrar">
-            Cadastrar
-          </button>
-          <button>Cancelar</button>
-
+          <button name="Cadastrar">Cadastrar</button>
+          <button onClick={ cancelarFormulario }>Cancelar</button>
         </form>
       )}
     </div>
