05dbe229df2efbfc652f6e0420c2175f4c27adc1
Samuel Sfeir <samuel1808@hotmail.com>
2023-11-30 10:26:41 -0300

    adiciona requisito 03

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,40 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+  const [exibirFormulario, setExibirFormulario] = useState(false);
+
+  const mostrarFormulario = () => {
+    setExibirFormulario(true);
+  };
+
   return (
-    <form className="form">
-      <label htmlFor="nome-do-servico">Nome do serviço</label>
-      <input
-        type="text"
-        name="nome-do-servico"
-        id="nome-do-servico"
-      />
+    <div>
+      <button onClick={ mostrarFormulario }>Cadastrar nova senha</button>
+      {exibirFormulario && (
+        <form className="form">
+          <label htmlFor="nome-do-servico">Nome do serviço</label>
+          <input
+            type="text"
+            name="nome-do-servico"
+            id="nome-do-servico"
+          />
 
-      <label htmlFor="login">Login</label>
-      <input
-        type="text"
-        name="login"
-        id="login"
-      />
+          <label htmlFor="login">Login</label>
+          <input
+            type="text"
+            name="login"
+            id="login"
+          />
 
-      <label htmlFor="senha">Senha</label>
-      <input
-        type="password"
-        name="senha"
-        id="senha"
-      />
+          <label htmlFor="senha">Senha</label>
+          <input
+            type="password"
+            name="senha"
+            id="senha"
+          />
 
-      <label htmlFor="URL">URL</label>
-      <input
-        type="text"
-        name="URL"
-        id="URL"
-      />
+          <label htmlFor="URL">URL</label>
+          <input
+            type="text"
+            name="URL"
+            id="URL"
+          />
 
-      <button>Cadastrar</button>
-      <button>Cancelar</button>
+          <button name="Cadastrar">
+            Cadastrar
+          </button>
+          <button>Cancelar</button>
 
-    </form>
+        </form>
+      )}
+    </div>
   );
 }
 
