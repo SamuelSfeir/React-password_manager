9e415194ad532e95342f808ad49e7a1f036709bd
Samuel Sfeir <samuel1808@hotmail.com>
2023-11-30 09:25:56 -0300

    adiciona requisito 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Title />
     </div>
   );
 }
---
+++ src/components/Title.tsx
@@ -0,0 +1,7 @@
+function Title() {
+  return (
+    <h1>Gerenciador de senhas</h1>
+  );
+}
+
+export default Title;
