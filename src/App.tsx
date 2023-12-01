2174c9257262d045101c934647a72851cb85f548
Samuel Sfeir <samuel1808@hotmail.com>
2023-12-01 19:09:45 -0300

    adiciona requisito 06

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
 import './App.css';
+import { useState } from 'react';
 import Title from './components/Title';
 import Form from './components/Form';
 
 function App() {
+  const [exibirFormulario, setExibirFormulario] = useState(false);
+
+  const toggleFormulario = () => {
+    setExibirFormulario(!exibirFormulario);
+  };
+
+  const [listaSenhas, setListaSenhas] = useState<{
+    nomeServico: string;
+    login: string;
+    senha: string;
+    url: string;
+  }[]>([]);
+
   return (
     <div>
       <Title />
-      <Form />
+      {!exibirFormulario && (
+        <>
+          <button onClick={ toggleFormulario }>
+            Cadastrar nova senha
+          </button>
+          {listaSenhas.length ? (
+            listaSenhas.map((lista) => (
+              <div key={ lista.nomeServico }>
+                <a href={ lista.url }>{lista.nomeServico}</a>
+                <p>{lista.login}</p>
+                <p>{lista.senha}</p>
+              </div>
+            ))
+          ) : (
+            <p>Nenhuma senha cadastrada</p>
+          )}
+        </>
+      )}
+      {exibirFormulario && <Form
+        setListaSenhas={ setListaSenhas }
+        toggleFormulario={ toggleFormulario }
+      /> }
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
 
-function Form() {
-  const [exibirFormulario, setExibirFormulario] = useState(false);
-  const [senhasCadastradas, setSenhasCadastradas] = useState(false);
+type FormProps = {
+  toggleFormulario: () => void
+  setListaSenhas: React.Dispatch<React.SetStateAction<{
+    nomeServico: string;
+    login: string;
+    senha: string;
+    url: string;
+  }[]>>
+};
+
+function Form(props: FormProps) {
+  const { toggleFormulario, setListaSenhas } = props;
 
   // Estados para cada campo do formulário
   const [nomeServico, setNomeServico] = useState('');
@@ -10,109 +19,93 @@ function Form() {
   const [senha, setSenha] = useState('');
   const [url, setURL] = useState('');
 
-  const toggleFormulario = () => {
-    setExibirFormulario(!exibirFormulario);
-  };
-
   const cancelarFormulario = () => {
-    setExibirFormulario(false);
+    toggleFormulario();
     // Limpar os estados dos campos ao cancelar o formulário
     setNomeServico('');
     setLogin('');
     setSenha('');
     setURL('');
   };
-  const contemNumeros = (str:string) => /\d/.test(str);
+  const contemNumeros = /\d/.test(senha);
   const contemLetras = (str:string) => /[a-zA-Z]/.test(str);
   const contemCaracteresEspeciais = (str:string) => /[!@#$%^&*(),.?":{}|<>]/.test(str);
 
-  const isDisabled = nomeServico.length === 0
-    || senha.length < 8
-    || senha.length > 16
-    || login.length === 0
-    || !contemNumeros(senha)
-    || !contemLetras(senha)
-    || !contemCaracteresEspeciais(senha);
-
   const isSenhaValida = senha.length >= 8 && senha.length <= 16;
-
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setSenhasCadastradas(true);
+    const formInfo = {
+      nomeServico,
+      login,
+      senha,
+      url,
+    };
+    setListaSenhas((prev) => ([...prev, formInfo]));
+    toggleFormulario();
   };
-
   return (
     <div>
-      {!exibirFormulario && !senhasCadastradas && (
-        <p>Nenhuma senha cadastrada</p>
-      )}
-      {!exibirFormulario && (
-        <button onClick={ toggleFormulario }>Cadastrar nova senha</button>
-      )}
-
-      {exibirFormulario && (
-        <form className="form" onSubmit={ handleSubmit }>
-          <label htmlFor="nome-do-servico">Nome do serviço</label>
-          <input
-            type="text"
-            name="nome-do-servico"
-            id="nome-do-servico"
-            value={ nomeServico }
-            onChange={ (e) => setNomeServico(e.target.value) }
-          />
-
-          <label htmlFor="login">Login</label>
-          <input
-            type="text"
-            name="login"
-            id="login"
-            value={ login }
-            onChange={ (e) => setLogin(e.target.value) }
-          />
-
-          <label htmlFor="senha">Senha</label>
-          <input
-            type="password"
-            name="senha"
-            id="senha"
-            value={ senha }
-            onChange={ (e) => setSenha(e.target.value) }
-          />
-          {isSenhaValida ? (
-            <p className="valid-password-check">Possuir 8 ou mais caracteres</p>
-          ) : (
-            <p className="invalid-password-check">
-              A senha deve ter entre 8 e 16 caracteres
-            </p>
-          )}
-          {contemCaracteresEspeciais(senha) ? (
-            <p className="valid-password-check">Possuir algum caractere especial</p>
-          ) : (
-            <p className="invalid-password-check">Possuir algum caractere especial</p>
-          )}
-          {contemLetras(senha) && contemNumeros(senha) ? (
-            <p className="valid-password-check">Possuir letras e números</p>
-          ) : (
-            <p className="invalid-password-check">Possuir letras e números</p>
-          )}
-
-          <label htmlFor="URL">URL</label>
-          <input
-            type="text"
-            name="URL"
-            id="URL"
-            value={ url }
-            onChange={ (e) => setURL(e.target.value) }
-          />
-
-          <button type="submit" name="Cadastrar" disabled={ isDisabled } id="cadastrar">
-            Cadastrar
-          </button>
-          <button type="button" onClick={ cancelarFormulario }>
-            Cancelar
-          </button>
-        </form>
-      )}
+      <form className="form" onSubmit={ handleSubmit }>
+        <label htmlFor="nome-do-servico">Nome do serviço</label>
+        <input
+          type="text"
+          name="nome-do-servico"
+          id="nome-do-servico"
+          value={ nomeServico }
+          onChange={ (e) => setNomeServico(e.target.value) }
+        />
+        <label htmlFor="login">Login</label>
+        <input
+          type="text"
+          name="login"
+          id="login"
+          value={ login }
+          onChange={ (e) => setLogin(e.target.value) }
+        />
+        <label htmlFor="senha">Senha</label>
+        <input
+          type="password"
+          name="senha"
+          id="senha"
+          value={ senha }
+          onChange={ (e) => setSenha(e.target.value) }
+        />
+        {isSenhaValida ? (
+          <p className="valid-password-check">Possuir 8 ou mais caracteres</p>
+        ) : (
+          <p className="invalid-password-check">
+            A senha deve ter entre 8 e 16 caracteres
+          </p>
+        )}
+        {contemCaracteresEspeciais(senha) ? (
+          <p className="valid-password-check">Possuir algum caractere especial</p>
+        ) : (
+          <p className="invalid-password-check">Possuir algum caractere especial</p>
+        )}
+        {contemLetras(senha) && contemNumeros ? (
+          <p className="valid-password-check">Possuir letras e números</p>
+        ) : (
+          <p className="invalid-password-check">Possuir letras e números</p>
+        )}
+        <label htmlFor="URL">URL</label>
+        <input
+          type="text"
+          name="URL"
+          id="URL"
+          value={ url }
+          onChange={ (e) => setURL(e.target.value) }
+        />
+        <button
+          type="submit"
+          name="Cadastrar"
+          id="cadastrar"
+        >
+          Cadastrar
+        </button>
+        <button type="submit" onClick={ cancelarFormulario }>
+          Cancelar
+        </button>
+      </form>
     </div>
   );
 }
